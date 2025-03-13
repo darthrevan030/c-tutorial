4171c01f291e32cfda5c81d1f3ecd71def178eea
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-13 17:12:39 +0800

    Bubble Sort Using Array Manipulation

---
+++ full-revision/bubbleSort_ArrayManipulation.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[12] = "Hello";
+
+    for(int i = 0; i < strlen(str) - 1; i++){
+        for(int j = 0; j < strlen(str) - 1; j++){
+            char temp = str[j];
+            str[j] = str[j+1];
+            str[j+1] = temp;
+        }
+    }
+    printf("%s", str);
+
+    return 0;
+}
