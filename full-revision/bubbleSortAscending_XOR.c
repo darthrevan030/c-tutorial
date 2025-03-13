c2b7cfa6a2673e04434f220001bc5a6c9027f5a4
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-13 20:05:47 +0800

    Changed from Descending to Ascending

--- full-revision/bubbleSortAscending_XOR.c
+++ full-revision/bubbleSortAscending_XOR.c
@@ -6,7 +6,7 @@ int main(){
 
     for(int i = 0; i < strlen(str) - 1; i++){
         for(int j = 0; j < strlen(str) - 1; j++){
-            if (str[j+1] < str[j]){
+            if (str[j+1] > str[j]){
             str[j] = str[j] ^ str[j+1];
             str[j+1] = str[j] ^ str[j+1];
             str[j] = str[j] ^ str[j+1];
